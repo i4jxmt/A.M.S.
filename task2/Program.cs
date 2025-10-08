1d9bf7b9d8c8b5209996e09353159f9ffb85a50c
user <user@WIN-7V1N9NBLNLF>
2025-10-08 20:28:13 +0300

    task02

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Вычисление длины окружности и площади круга");
+
+        Console.Write("Введите радиус:");
+        double R = Convert.ToDouble(Console.ReadLine());
+
+        double C = 2 * Math.PI * R;
+        double S = Math.PI * Math.Pow(R, 2);
+
+        Console.WriteLine($"Длина окружности: {C:F2}");
+        Console.WriteLine($"Площадь круга: {S:F2}");
+    }
+}
