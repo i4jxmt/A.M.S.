efa683300a0a1b3e44dfe2a0b8d884bb75ba77af
user <user@WIN-7V1N9NBLNLF>
2025-10-15 15:12:56 +0300

    task05

---
+++ task05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("вычисление значения x");
+
+        double fraction1 = CalculateFraction(5, 7);
+        double fraction2 = CalculateFraction(12, 8);
+        double fraction3 = CalculateFraction(31, 2);
+
+        double x = fraction1 * fraction2 * fraction3;
+        Console.WriteLine($"x = {x:F3}");
+    }
+
+    static double CalculateFraction(int a, int b)
+    {
+        double N = a + Math.Sqrt(a);
+        double D = b + Math.Sqrt(b);
+        return N / D;
+    }
+}
